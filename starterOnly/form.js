6e7e4c535752f0f92eb0c2fa863429729fa2789a
Fabien <pic.fab@hotmail.fr>
2021-02-09 18:02:56 +0100

    features: ajout du nettoyage du formulaire après envoie du formulaire

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -2,14 +2,18 @@ const modalContent = document.querySelector(".modal-body");
 const formInscription = document.getElementById("formInscription");
 const submit = document.getElementById("submitIncription");
 const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelectorAll(".close");
-//Create a object for the inscription form and validate, display error and succes answer
-// need all formData in the constructor
-// send validate or error after submission
+const closeBtn = document.querySelector(".close");
+let modalContentHeight;
 
+/**
+ * Créer un objet pour le formulaire d'inscription qui permet de valider,
+ * d'afficher les erreur ainsi que le message de succés après la soumission.
+ */
 class formIncription{
 
-  //all error and success answer
+  /**
+   * Tous les messages d'erreur et de validation
+   */
   message={
     first:"Veuillez entrer 2 caractères ou plus pour le champ du Prénom.",
     last:"Veuillez entrer 2 caractères ou plus pour le champ du Nom.",
@@ -21,14 +25,21 @@ class formIncription{
     success:"Merci ! Votre réservation a été reçue.",
   }
 
-  // Constructor
+  /**
+   *
+   * @param {object} formData Nodelist avec tous les élements du dom contenant la class CSS .formData
+   */
   constructor(formData){
     this.formData=formData
     this.setValues(formData)
   }
 
   // set all inputs value
-  setValues(formData) {
+  /**
+   *
+   * @param {object} formData Nodelist avec tous les éléments comprenant la classe CSS .formData
+   */
+  setValues=(formData)=>{
     formData.forEach(elt => {
         const eltInputs = elt.getElementsByTagName('input')
         Array.from(eltInputs).forEach(input => {
@@ -61,8 +72,14 @@ class formIncription{
     })
   }
 
-  // set one input value
-  setValue(name,value,type=null,error=false){
+  /**
+   * Paramétrer la valeur d'un input
+   * @param {string} name
+   * @param {*} value
+   * @param {string} type
+   * @param {boolean} error
+   */
+  setValue=(name,value,type=null,error=false)=>{
     this.fields= {
       ...this.fields,
       [name]:{
@@ -75,7 +92,12 @@ class formIncription{
   }
 
   //set one error
-  setError(key,val){
+  /**
+   *
+   * @param {number} key
+   * @param {boolean} val
+   */
+  setError=(key,val)=>{
       this.fields= {
         ...this.fields,
         [key]:{
@@ -85,8 +107,10 @@ class formIncription{
       }
   }
 
-  // verify all errors
-  verifErrors(){
+  /**
+   * Vérifier toutes les erreurs
+   */
+  verifErrors=()=>{
     let error=false
     Object.entries(this.fields).forEach(
       ([key, item]) => {
@@ -101,9 +125,11 @@ class formIncription{
     return error
   }
 
-  //show one error
-  showError(name){
-    console.log(name);
+  /**
+   * Affiche une erreur
+   * @param {string} name nom de l'erreur
+   */
+  showError=(name)=>{
     this.removeError(name)
     const formBox = formInscription.querySelector(`[name=${name}]`)
     const elt = formBox.closest('.formData')
@@ -113,7 +139,6 @@ class formIncription{
     content.classList.add(name)
     content.innerHTML=this.message[name]
     if(name==='accept'){
-
       const acceptFormbox =elt.querySelector(`[for=${formBox.id}]`)
       acceptFormbox.after(content)
     }else{
@@ -122,7 +147,11 @@ class formIncription{
   }
 
   //remove one error
-  removeError(name){
+  /**
+   * supprime une erreur
+   * @param {string} name nom de l'erreur
+   */
+  removeError=(name)=>{
     const oldElt = formInscription.querySelector(`.error.${name}`)
     if(oldElt){
       oldElt.parentNode.closest(".error").classList.remove('error')
@@ -130,33 +159,63 @@ class formIncription{
     }
   }
 
-  //show success message
-  removeSucces(){
-    console.log(formInscription)
+  /**
+   * supprime le message de succés
+   */
+  removeSuccess=()=>{
     modalContent.innerHTML=''
+    modalContent.removeAttribute('style')
+    modalContent.classList.remove('success')
     modalContent.append(formInscription)
+    closeBtn.removeEventListener("click", this.removeSuccess);
+    closeModal()
   }
-  showSuccess(){
-    modalContent.innerHTML = `<span>${this.message.success}</span>`
+
+  /**
+   * Netoie le formulaire
+   */
+  clean=()=>{
+    // Nettoie le formulaire
+    Object.entries(formData).forEach(
+      ([key, item]) => {
+        const inputs = item.getElementsByTagName('input')
+        Array.from(inputs).forEach(input => {
+          if (['checkbox', 'radio'].includes(input.type)) {
+            if ("accept" !== input.name) {
+              input.checked = false
+            }
+          } else {
+            input.value = null
+          }
+        });
+      })
+  }
+  /**
+   * Affiche le message de succés
+   */
+  showSuccess=()=>{
+    modalContentHeight = modalContent.offsetHeight
+    modalContent.style.minHeight = modalContentHeight+'px';
+    modalContent.classList.add('success')
+    modalContent.innerHTML = `<div class="successBox"><span>${this.message.success}</span></div>`
 
     const buttonClose = document.createElement('button')
-    buttonClose.classList.add('btn-submit')
+    buttonClose.classList.add('btn-submit', 'button')
     buttonClose.classList.add('button')
-        buttonClose.textContent='Fermer'
+    buttonClose.textContent='Fermer'
 
     modalContent.append(buttonClose)
-    buttonClose.onclick = ()=>{
-      this.removeSucces()
-      closeModal()
+    buttonClose.onclick = () => {
+      this.removeSuccess()
     }
-    closeBtn.onclick=()=>{
-      this.removeSucces()
-      closeModal()
-    }
+    closeBtn.addEventListener("click", this.removeSuccess);
+    this.clean()
   }
 
-  //validate all inputs fields
-  validate(){
+  /**
+   * Vérifie que tous les inputs sont bien remplis
+   */
+  validate=()=>{
     const regexpAlpha = new RegExp(/^[a-zéèàùûêâçôë]{1}[a-zéèàùûêâçôë \'-]*[a-zéèàùûêâçôë]$/i)
     const regexpEmail = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)
     const regexpNumber = new RegExp(/^[0-9]+$/)
@@ -186,17 +245,23 @@ class formIncription{
   }
 }
 
-// instance of formInscription
+/**
+ * Instancie l'objet formulaire "formIncription"
+ */
 const form = new formIncription(formData,['first','last','email','birthdate','location','accept','event'])
 
-// prevent submition by default
+/**
+ * Empêche la soumission par default
+ */
 formInscription.addEventListener("submit", function(evt) {
         evt.preventDefault();
 }, false);
 
-// function validate in "onsubmit" into the form html
-
-function validate(toto){
+/**
+ * Lance la validation du formulaire
+ * lorsque l'utilisateur click sur le bouton c'est part
+ */
+function validate(){
     form.setValues(formData)
     if(form.validate()){
       form.showSuccess()
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,6 @@
+/**
+ * Ajoute ou supprime la class responsive lorsqu'on click sur l'icon hamburger
+ */
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -10,16 +13,33 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const body = document.querySelector('body')
+const menu = document.querySelector(".topnav");
 
-// launch modal event
+/**
+ * Ajout l'évenement click pour ouvrir la modal
+ */
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+/**
+ * Ouvre la modal
+ */
 function launchModal() {
   modalbg.style.display = "block";
+  body.style.overflowY = 'hidden'
+  if(body.offsetWidth<'540'){
+    modalbg.prepend(menu)
+  }
 }
 
-//close modal form
+/**
+ * Ferme la modal
+ */
 function closeModal() {
   modalbg.style.display = "none";
+  body.removeAttribute('style');
+
+  if(body.offsetWidth<'540'){
+    body.prepend(menu)
+  }
 }
